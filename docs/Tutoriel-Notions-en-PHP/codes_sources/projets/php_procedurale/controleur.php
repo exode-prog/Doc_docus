c6df9a48dd2a5694884f691ec99b584700dca82a
exode-prog <exode4141@gmail.com>
2024-10-02 17:36:12 +0000

    Delete docs/Tutoriel-Notions-en-PHP/codes_sources/projets directory

--- docs/Tutoriel-Notions-en-PHP/codes_sources/projets/php_procedurale/controleur.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'modele.php';
-require_once 'vue.php';
-
-
-
-?>
--- docs/Tutoriel-Notions-en-PHP/codes_sources/projets/tabb/controleur_details_etudiant.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  require_once 'modele.php';
-  $person = details_etudiant_par_id($_GET['id']);
-  require 'templates/details_etudiant_layout.php';
-?>
-
